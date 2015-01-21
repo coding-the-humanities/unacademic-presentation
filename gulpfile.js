6e0999ff6ac6c6aaf2cbcaf3559ba9b82605193b
Zaturrby <rjkorteschiel@gmail.com>
2015-01-21 15:46:59 +0100

    getting there, only live reload and clean left

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,53 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
-gulp.task('jshint', function () {
+gulp.task('scripts', function () {
     gulp.src(['src/scripts/**/*.js'])
         .pipe($.jshint())
-        .pipe($.jshint.reporter('jshint-stylish'));
+        .pipe($.jshint.reporter('jshint-stylish'))
+        .pint(gulp.dest('dist'));
 });
 
-gulp.task('default', ['jshint'], function() {
-  // place code for your default task here
+gulp.task('styles', function () {
+    gulp.src('src/styles/**/*.scss')
+        .pipe($.sass({
+        	includePaths: require('node-neat').includePaths
+        }))
+        .pipe(gulp.dest('dist/'));
 });
+
+gulp.task('html', function() {  
+	gulp.src(['src/**/*.html'])
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function() {  
+	gulp.src(['src/images/**'])
+		.pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('fonts', function() {  
+	gulp.src(['src/fonts/**'])
+		.pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('build', ['scripts', 'styles', 'html', 'images', 'fonts'], function() {  
+
+});
+
+gulp.task('serve', ['build'], function(){ 
+    gulp.watch(['src/**/*.html'], ['html']);
+    gulp.watch(['src/styles/**/*.{scss,css}'], ['styles']);
+    gulp.watch(['src/scripts/**/*.js'], ['jshint']);
+    gulp.watch(['src/images/**/*'], ['images']);
+});
+
+gulp.task('default', ['serve'], function() {
+	
+});
+
+gulp.task('deploy', ['default'], function () {
+  return gulp.src('./dist/**/*')
+    .pipe($.deploy());
+});
+
